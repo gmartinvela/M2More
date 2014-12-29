0386bdc46f8d1a9c3eb605d3ce2b0d5b06a9513b
gmartinvela <gustavo.martin@opendeusto.es>
2014-12-29 13:42:09 +0100

    Services working

--- services.py
+++ services.py
@@ -3,15 +3,18 @@
 
 import tornado.ioloop
 import tornado.web
+import datetime
 
 class MainHandler(tornado.web.RequestHandler):
-	def get(self):
-		self.write("Hello, world")
+    def get(self):
+        now = datetime.datetime.now()
+        pretty_now = now.strftime("%d-%m-%Y %H:%M")
+        self.write("<p>RPI Bilbao</p><p>" + pretty_now  + "</p>")
 
 application = tornado.web.Application([
-	(r"/", MainHandler),
+    (r"/", MainHandler),
 ])
 
 if __name__ == "__main__":
-	application.listen(80)
-	tornado.ioloop.IOLoop.instance().start()
+    application.listen(80)
+    tornado.ioloop.IOLoop.instance().start()
